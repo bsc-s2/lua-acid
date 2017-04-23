d855d9a4dadc639c4ff7c455a8f4c4b58e3dd7f9
drdr xp <drdr.xp@gmail.com>
2017-04-23 21:40:17 +0800

    add net.lua: parse_ip_regex and choose

---
+++ lib/acid/net.lua
@@ -0,0 +1,97 @@
+local strutil = require("acid.strutil")
+
+local strutil_strip = strutil.strip
+local strutil_split = strutil.split
+
+local _M = { _VERSION = "0.1" }
+
+
+function _M.parse_ip_regexs(ip_regexs_str)
+
+    ip_regexs_str = strutil_strip(ip_regexs_str)
+
+    local regs = strutil_split(ip_regexs_str, ",")
+    local rst = {}
+    for _, r in ipairs(regs) do
+        if strutil.startswith(r, '-') then
+            r = {r:sub(2), false}
+        else
+            r = {r, true}
+        end
+
+        if r[1] == '' then
+            error('invalid ip regex: ' .. ip_regexs_str)
+        end
+
+        if r[2] then
+            r = r[1]
+        end
+
+        table.insert(rst, r)
+    end
+
+    return rst
+end
+
+
+function _M.choose_by_regex(ips, ip_regexs)
+
+    --[[
+    Choose ips that matches any of positive regex(without minus"-"), and does
+    not match all of negative regex(with minus"-").
+
+    eg.:
+    -   '-127[.]'
+        returns any non-localhost ip
+
+    -   '192[.],10[.],-.*[.]1,-.*[.]3'
+        returns intra net ip that starts with "192." or "10.", but not ends
+        with ".1" or ".3.
+    ]]
+
+    local rst = {}
+
+    for _, ip in ipairs(ips) do
+
+        local all_negative = true
+        local matched = false
+
+        for _, ip_regex in ipairs(ip_regexs) do
+
+            local reg, to_choose
+            if type(ip_regex) == 'table' then
+                reg = ip_regex[1]
+                to_choose = ip_regex[2]
+            else
+                reg = ip_regex
+                to_choose = true
+            end
+
+            all_negative = all_negative and not to_choose
+
+            if string.match(ip, '^' .. reg) then
+                matched = true
+                if to_choose then
+                    table.insert(rst, ip)
+                end
+                break
+            end
+        end
+
+        -- If there is not a positive regex, there is no chance for an ip to
+        -- be added into `rst`.
+        -- Thus we need to check if there are only negative regexs and add
+        -- it.
+
+        if not matched then
+            if all_negative then
+                table.insert(rst, ip)
+            end
+        end
+    end
+
+    return rst
+end
+
+
+return _M
---
+++ lib/test_net.lua
@@ -0,0 +1,100 @@
+local net = require('acid.net')
+
+local dd = test.dd
+
+
+function test.parse_ip_regexs(t)
+
+    local cases = {
+        {'1.2.3.4',          {'1.2.3.4'}},
+        {'1.2.3.4,127.0.',   {'1.2.3.4', '127.0.'}},
+        {'-1.2.3.4,127.0.',  {{'1.2.3.4', false}, '127.0.'}},
+        {'-1.2.3.4,-127.0.', {{'1.2.3.4', false}, {'127.0.', false}}},
+    }
+
+    for ii, c in ipairs(cases) do
+
+        local inp, expected = unpack(c)
+
+        local msg = 'case: ' .. tostring(ii) .. '-th '
+        dd(msg, c)
+
+        local rst = net.parse_ip_regexs(inp)
+        dd('rst: ', rst)
+
+        t:eqdict(expected, rst, msg)
+    end
+end
+
+
+function test.parse_ip_regexs_err(t)
+
+    local cases = {
+        {'',},
+        {',',},
+        {' , ',},
+        {'1,',},
+        {',1',},
+        {'-1,',},
+        {',-1',},
+        {'127,-',},
+        {'-,127',},
+    }
+
+    for ii, c in ipairs(cases) do
+
+        local inp = unpack(c)
+
+        local msg = 'case: ' .. tostring(ii) .. '-th '
+        dd(msg)
+
+        t:err(function ()
+            net.parse_ip_regexs(inp)
+        end, msg)
+
+    end
+end
+
+
+function test.choose_by_regex(t)
+
+    local cases = {
+        {{'127.0.0.1', '192.168.0.1'}, {'127[.]'},
+         {'127.0.0.1'}},
+
+        {{'127.0.0.1', '192.168.0.1'}, {'2'},
+         {}},
+
+        {{'127.0.0.1', '192.168.0.1'}, {'[.]'},
+         {}},
+
+        {{'127.0.0.1', '192.168.0.1'}, {'1'},
+         {'127.0.0.1', '192.168.0.1'}},
+
+        -- negative match
+        {{'127.0.0.1', '192.168.0.1'}, {{'1', false}},
+         {}},
+
+        {{'127.0.0.1', '192.168.0.1'}, {{'127', false}, {'192', false}},
+         {}},
+
+        {{'127.0.0.1', '192.168.0.1'}, {{'12', false}},
+         {'192.168.0.1'}},
+
+        {{'127.0.0.1', '192.168.0.1'}, {'22', {'12', false}},
+         {}},
+    }
+
+    for ii, c in ipairs(cases) do
+
+        local ips, regexs, expected = unpack(c)
+
+        local msg = 'case: ' .. tostring(ii) .. '-th '
+        dd(msg, c)
+
+        local rst = net.choose_by_regex(ips, regexs)
+        dd('rst: ', rst)
+
+        t:eqdict(expected, rst, msg)
+    end
+end
