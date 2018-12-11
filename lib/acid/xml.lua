b9fe66d298598da9588c1c614ca61f10aa98c4ed
slasher <mcq.sejust@gmail.com>
2018-12-11 17:44:54 +0800

    acid.xml using local functions

--- lib/acid/xml.lua
+++ lib/acid/xml.lua
@@ -2,6 +2,21 @@ local strutil = require('acid.strutil')
 local tableutil = require('acid.tableutil')
 local utf8 = require('acid.utf8')
 
+local type = type
+local pairs = pairs
+local ipairs = ipairs
+local tonumber = tonumber
+local tostring = tostring
+local string_byte = string.byte
+local string_char = string.char
+local string_sub = string.sub
+local string_gsub = string.gsub
+local string_find = string.find
+local string_match = string.match
+local string_format = string.format
+local table_concat = table.concat
+local table_insert = table.insert
+
 local _M = {}
 
 local DECLARATION = '<?xml version="1.0" encoding="UTF-8"?>'
@@ -34,10 +49,10 @@ function _M.xml_unescape(str)
         end
 
         -- &#123; or &#x2F;
-        local number_str = string.sub(entity_ref, 3, -2)
+        local number_str = string_sub(entity_ref, 3, -2)
         local number
-        if string.sub(number_str, 1, 1) == 'x' then
-            number = tonumber(string.sub(number_str, 2), 16)
+        if string_sub(number_str, 1, 1) == 'x' then
+            number = tonumber(string_sub(number_str, 2), 16)
         else
             number = tonumber(number_str)
         end
@@ -49,7 +64,7 @@ function _M.xml_unescape(str)
         return utf8.char({number})
     end
 
-    local unescaped = string.gsub(str, '&.-;', entity_ref_to_str)
+    local unescaped = string_gsub(str, '&.-;', entity_ref_to_str)
 
     return unescaped
 end
@@ -62,15 +77,15 @@ local function escape_by_char(str)
             return char_to_entity[c]
         end
 
-        local byte = string.byte(c)
+        local byte = string_byte(c)
         if byte >= 0x20 and byte < 0x7F then
             return c
         end
 
-        return string.format('&#x%02X;', byte)
+        return string_format('&#x%02X;', byte)
     end
 
-    local escaped_str = string.gsub(str, '.', escape_char)
+    local escaped_str = string_gsub(str, '.', escape_char)
 
     return escaped_str
 end
@@ -88,18 +103,18 @@ function _M.xml_safe_encode(str)
     for _, cp in ipairs(code_points) do
         if cp < 0x80 then
 
-            local c = string.char(cp)
+            local c = string_char(cp)
             if char_to_entity[c] ~= nil then
                 c = char_to_entity[c]
             end
 
-            table.insert(escaped_strs, c)
+            table_insert(escaped_strs, c)
         else
-            table.insert(escaped_strs, string.format('&#%d;', cp))
+            table_insert(escaped_strs, string_format('&#%d;', cp))
         end
     end
 
-    return table.concat(escaped_strs)
+    return table_concat(escaped_strs)
 end
 
 
@@ -109,15 +124,15 @@ local function build_tag(tag_name, tbl)
         local escaped_k = _M.xml_safe_encode(tostring(k))
         local escaped_v = _M.xml_safe_encode(tostring(v))
 
-        table.insert(attr, string.format(' %s="%s"', escaped_k, escaped_v))
+        table_insert(attr, string_format(' %s="%s"', escaped_k, escaped_v))
     end
 
-    local attr_str = table.concat(attr)
+    local attr_str = table_concat(attr)
 
     local escaped_tag_name = _M.xml_safe_encode(tostring(tag_name))
 
-    local open_tag = string.format('<%s%s>', escaped_tag_name, attr_str)
-    local close_tag = string.format('</%s>', escaped_tag_name)
+    local open_tag = string_format('<%s%s>', escaped_tag_name, attr_str)
+    local close_tag = string_format('</%s>', escaped_tag_name)
 
     return {open_tag=open_tag, close_tag=close_tag}
 end
@@ -144,12 +159,12 @@ function _M.build_dict_lines(lines, tag_name, tbl, indent_str, opts)
     local next_indent_str = indent_str .. (opts.indent or '')
 
     local tag = build_tag(tag_name, tbl)
-    table.insert(lines, indent_str .. tag.open_tag)
+    table_insert(lines, indent_str .. tag.open_tag)
 
     local keys = {}
     for k, _ in pairs(tbl) do
         if k ~= ATTR and k ~= KEY_ORDER then
-            table.insert(keys, k)
+            table_insert(keys, k)
         end
     end
 
@@ -165,7 +180,7 @@ function _M.build_dict_lines(lines, tag_name, tbl, indent_str, opts)
         end
     end
 
-    table.insert(lines, indent_str .. tag.close_tag)
+    table_insert(lines, indent_str .. tag.close_tag)
     return true, nil, nil
 end
 
@@ -179,14 +194,14 @@ function _M.build_lines(lines, tag_name, tbl, indent_str, opts)
 
     if #tbl == 1 and type(tbl[1]) ~= 'table' then
         local tag = build_tag(tag_name, tbl)
-        table.insert(lines, indent_str .. tag.open_tag)
+        table_insert(lines, indent_str .. tag.open_tag)
 
         local text = tbl[1]
 
         local encoded_text = _M.xml_safe_encode(tostring(text))
-        table.insert(lines, next_indent_str .. encoded_text)
+        table_insert(lines, next_indent_str .. encoded_text)
 
-        table.insert(lines, indent_str .. tag.close_tag)
+        table_insert(lines, indent_str .. tag.close_tag)
         return true, nil, nil
     end
 
@@ -214,7 +229,7 @@ function _M.to_xml(root_name, tbl, opts)
     local lines = {}
 
     if opts.no_declaration ~= true then
-        table.insert(lines, DECLARATION)
+        table_insert(lines, DECLARATION)
     end
 
     local _, err, errmsg = _M.build_lines(lines, root_name, tbl, '', opts)
@@ -227,7 +242,7 @@ function _M.to_xml(root_name, tbl, opts)
         concatenator = '\n'
     end
 
-    local str = table.concat(lines, concatenator)
+    local str = table_concat(lines, concatenator)
 
     return str, nil, nil
 end
@@ -238,13 +253,13 @@ local function parse_tag_body(str, body_segment)
         return {}
     end
 
-    local _, open_tag_e = string.find(str, open_tag_ptn,
+    local _, open_tag_e = string_find(str, open_tag_ptn,
                                       body_segment.start_i)
     if open_tag_e ~= nil and open_tag_e <= body_segment.end_i then
         return nil
     end
 
-    local body_str = string.sub(str, body_segment.start_i,
+    local body_str = string_sub(str, body_segment.start_i,
                                 body_segment.end_i)
     return _M.xml_unescape(body_str)
 end
@@ -252,7 +267,7 @@ end
 
 local function parse_attr(attr_str)
     local attrs_raw = {}
-    string.gsub(attr_str, '([^%s]+)%s*=%s*([^%s]+)',
+    string_gsub(attr_str, '([^%s]+)%s*=%s*([^%s]+)',
                 function(name, value) attrs_raw[name] = value end)
 
     local attrs = {}
@@ -277,26 +292,26 @@ local function parse_open_tag(open_tag)
     local name_attr
 
     -- <foo /> or <foo a="b">
-    if string.sub(open_tag, -2, -2) == '/' then
+    if string_sub(open_tag, -2, -2) == '/' then
         open_tag_info.is_empty_tag = true
-        name_attr = string.sub(open_tag, 2, -3)
+        name_attr = string_sub(open_tag, 2, -3)
     else
         open_tag_info.is_empty_tag = false
-        name_attr = string.sub(open_tag, 2, -2)
+        name_attr = string_sub(open_tag, 2, -2)
     end
 
-    local space_i, _ = string.find(name_attr, ' ')
+    local space_i, _ = string_find(name_attr, ' ')
     space_i = space_i or (#name_attr + 1)
 
-    open_tag_info.tag_name_raw = string.sub(name_attr, 1, space_i - 1)
+    open_tag_info.tag_name_raw = string_sub(name_attr, 1, space_i - 1)
     if open_tag_info.tag_name_raw == '' then
-        return nil, 'InvalidXML', string.format(
+        return nil, 'InvalidXML', string_format(
                 'tag name is empty string in: %s', open_tag)
     end
 
     open_tag_info.tag_name = _M.xml_unescape(open_tag_info.tag_name_raw)
 
-    local attr_str = string.sub(name_attr, space_i + 1, #name_attr)
+    local attr_str = string_sub(name_attr, space_i + 1, #name_attr)
     local attrs, err, errmsg = parse_attr(attr_str)
     if err ~= nil then
         return nil, err, errmsg
@@ -309,29 +324,29 @@ end
 
 
 local function parse_one_element(str, segment)
-    local to_parse = string.sub(str, segment.start_i, segment.end_i)
-    if string.match(to_parse, '^%s*$') then
+    local to_parse = string_sub(str, segment.start_i, segment.end_i)
+    if string_match(to_parse, '^%s*$') then
         return nil, nil, nil
     end
 
-    local open_tag_s, open_tag_e = string.find(str, open_tag_ptn,
+    local open_tag_s, open_tag_e = string_find(str, open_tag_ptn,
                                                segment.start_i)
     if open_tag_e == nil or open_tag_e > segment.end_i then
-        return nil, 'InvalidXML', string.format(
+        return nil, 'InvalidXML', string_format(
                 'can not find open tag at: %s... within: %d chars',
-                string.sub(str, segment.start_i, segment.start_i + 100),
+                string_sub(str, segment.start_i, segment.start_i + 100),
                 segment.end_i - segment.start_i + 1)
     end
 
-    local pre_open_tag = string.sub(str, segment.start_i, open_tag_s - 1)
-    if not string.match(pre_open_tag, '^%s*$') then
-        return nil, 'InvalidXML', string.format(
+    local pre_open_tag = string_sub(str, segment.start_i, open_tag_s - 1)
+    if not string_match(pre_open_tag, '^%s*$') then
+        return nil, 'InvalidXML', string_format(
                 'find extra string: %s... before open tag',
-                string.sub(pre_open_tag, 1, 100))
+                string_sub(pre_open_tag, 1, 100))
     end
 
     local open_tag_info, err, errmsg = parse_open_tag(
-            string.sub(str, open_tag_s, open_tag_e))
+            string_sub(str, open_tag_s, open_tag_e))
     if err ~= nil then
         return nil, err, errmsg
     end
@@ -347,13 +362,13 @@ local function parse_one_element(str, segment)
         return element, nil, nil
     end
 
-    local close_tag = string.format('</%s>', open_tag_info.tag_name_raw)
-    local close_tag_s, close_tag_e = string.find(str, close_tag,
+    local close_tag = string_format('</%s>', open_tag_info.tag_name_raw)
+    local close_tag_s, close_tag_e = string_find(str, close_tag,
                                                  open_tag_e + 1, true)
     if close_tag_e == nil or close_tag_e > segment.end_i then
-        return nil, 'InvalidXML', string.format(
+        return nil, 'InvalidXML', string_format(
                 'can not find close tag: %s at: %s...', close_tag,
-                string.sub(str, open_tag_e + 1, open_tag_e + 101))
+                string_sub(str, open_tag_e + 1, open_tag_e + 101))
     end
 
     segment.start_i = close_tag_e + 1
@@ -395,7 +410,7 @@ function _M.parse_xml(tree, str, segment)
             tree[tag_name][ATTR] = element[ATTR]
         end
 
-        table.insert(tree[tag_name], element.value)
+        table_insert(tree[tag_name], element.value)
     end
 
     return true, nil, nil
@@ -433,8 +448,8 @@ end
 
 
 function _M.from_xml(str)
-    str = string.gsub(str, '<!--(.-)-->', '')
-    str = string.gsub(str, '<?(.-)?>', '')
+    str = string_gsub(str, '<!--(.-)-->', '')
+    str = string_gsub(str, '<?(.-)?>', '')
     local segment = {
         start_i = 1,
         end_i = #str,
